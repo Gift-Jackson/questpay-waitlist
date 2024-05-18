1c240368518b789a12c61275ad5ff28b4be42108
Gift Jacksun <giftjacksun@gmail.com>
2024-05-18 15:20:28 +0100

    env import wahala

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -4,11 +4,16 @@ export const project_id = import.meta.env.PROJECT_ID
 export const COLLECTION_ID = "6648995000241a2b64ec"
 export const DATABASE_ID = "6648993c0015bec1322a"
 
+const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT
+console.log(endpoint)
+
+
 const client = new Client();
 
 client
-.setEndpoint(import.meta.env.vITE_APPWRITE_ENDPOINT)
-.setProject(import.meta.env.vITE_APPWRITE_ENDPOINT);
+.setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_PROJECT_ID);
+
 
 export const databases = new Databases(client);
 export default client
